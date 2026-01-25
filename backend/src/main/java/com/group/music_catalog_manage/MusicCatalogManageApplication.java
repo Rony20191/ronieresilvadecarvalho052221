db7676b7b2cde481a6b38b78cf13aa8b9602bf3a
ronierecarvalho <ronierecarvalho@ses.mt.gov.br>
2026-01-25 17:56:24 -0300

    feat: criação inicial projeto backend

---
+++ backend/src/main/java/com/group/music_catalog_manage/MusicCatalogManageApplication.java
@@ -0,0 +1,13 @@
+package com.group.music_catalog_manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicCatalogManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicCatalogManageApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/group/music_catalog_manage/MusicCatalogManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.music_catalog_manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicCatalogManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
